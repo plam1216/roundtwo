cb21208e545852bce0886fd7a411b0b88beebb47
Peter Lam <plam1216@gmail.com>
2022-10-14 00:43:47 -0400

    schemas created

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const listingSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    condition: { type: String },
+    category: { type: String, required: true },
+    size: { type: String, required: true },
+    price: { type: Number, required: true, min: 1 },
+    description: String,
+    image: { type: String, required: true }
+}, {
+    timestamps: true
+})
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    listings: [listingSchema],
+    bio: String
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
