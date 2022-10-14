8913461bfd2abbdde52b5a1537758a61981e43d3
Peter Lam <plam1216@gmail.com>
2022-10-13 23:22:23 -0400

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+///////////////
+// DEPENDENCIES
+///////////////
+const express = require('express')
+const app = express()
+
+// get .env variables
+require('dotenv').config()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const PORT = process.env.PORT || 4000
+
+//////////////
+// MIDDLEWARE
+//////////////
+app.use(cors()) // to prevent cors errors, allows access to all origins
+app.use(morgan('dev')) // logging
+app.use(express.json()) // parse json bodies
+
+///////////////////////
+// DATABASE CONNECTION
+///////////////////////
+mongoose.connect(process.env.MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewURLParser: true
+})
+
+const db = mongoose.connection
+db.on('connected',() => console.log('Connected to MongoDB'))
+db.on('disconnected',() => console.log('Disconnected from MongoDB'))
+db.on('error',(err) => console.log(err + 'error connecting to MongoDB'))
+
+
+//////////
+// ROUTES
+//////////
+app.get('/', (req, res) => {
+    res.send('Route is working!')
+})
+
+
+/////////////
+// LISTENING
+/////////////
+app.listen(PORT, () => {
+    console.log('Listening on PORT: ', PORT)
+})
