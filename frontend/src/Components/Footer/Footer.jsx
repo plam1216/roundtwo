3b84f956acb4b8210818f81f07a754a002a7f27d
Peter Lam <plam1216@gmail.com>
2022-10-18 01:34:51 -0400

    footer

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="page-footer">
+      {/* <div class="footer-copyright"> */}
+        {/* <div class="container"> */}
+        <div>
+          © 2022 Copyright <br/>
+          RoundTwo; Online Marketplace to Buy and Sell Clothes
+        </div>
+      {/* </div> */}
+    </footer>
+  )
+}
+
+export default Footer
