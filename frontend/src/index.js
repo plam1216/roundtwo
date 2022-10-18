d0bd246b171492309bc12310a3e7c4fef37f208e
Peter Lam <plam1216@gmail.com>
2022-10-18 01:37:36 -0400

    link css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Routes from './Routes.js'
+
+import './main.scss'
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes />
+    </Router>
   </React.StrictMode>
 );
 
