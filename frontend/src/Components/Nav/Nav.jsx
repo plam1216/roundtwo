c2826a05ddb62be552a488afe781e1292ada6b5a
Peter Lam <plam1216@gmail.com>
2022-11-11 18:09:54 -0500

    add login logout

--- frontend/src/Components/Nav/Nav.jsx
+++ frontend/src/Components/Nav/Nav.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { login, logout } from '../../services/firebase'
 
-const Nav = () => {
+const Nav = ({ user }) => {
   return (
-    <>
-      <nav>
-        <div className="nav-wrapper">
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <nav>
+      <div className="nav-wrapper">
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/shop'>Shop</Link>
+          </li>
+          {user ?
+            <>
+              <li>
+                <Link to='/sell'>Sell</Link>
+              </li>
+              <li onClick={logout}>
+                <Link to='/' onClick={logout}>Logout</Link>
+              </li>
+            </>
+            :
             <li>
-              <Link to='/'>Home</Link>
+              <Link to='/' onClick={login}>Login</Link>
             </li>
-            <li>
-              <Link to='/shop'>Shop</Link>
-            </li>
-            <li>
-              <Link to='/sell'>Sell</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+          }
+          {user ?
+            <img src={user.photoURL} alt={user.displayName} />
+            :
+            null
+          }
+        </ul>
+      </div>
+    </nav>
   )
 }
 
