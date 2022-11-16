c975ad83838390c984f1a73650a5e5c6c6edbca3
Peter Lam <plam1216@gmail.com>
2022-11-15 20:04:09 -0500

    add nav styling

--- frontend/src/Components/Nav/Nav.jsx
+++ frontend/src/Components/Nav/Nav.jsx
@@ -4,35 +4,49 @@ import { login, logout } from '../../services/firebase'
 
 const Nav = ({ user }) => {
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/shop'>Shop</Link>
-          </li>
-          {user ?
-            <>
-              <li>
-                <Link to='/sell'>Sell</Link>
-              </li>
-              <li onClick={logout}>
-                <Link to='/' onClick={logout}>Logout</Link>
-              </li>
-            </>
-            :
-            <li>
-              <Link to='/' onClick={login}>Login</Link>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid justify-content-end">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul id="nav-mobile" className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>Home</Link>
             </li>
-          }
-          {user ?
-            <img src={user.photoURL} alt={user.displayName} />
-            :
-            null
-          }
-        </ul>
+            <li className="nav-item">
+              <Link className="nav-link" to='/shop'>Shop</Link>
+            </li>
+            {user ?
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to='/sell'>Sell</Link>
+                </li>
+                <li className="nav-item" onClick={logout}>
+                  <Link className="nav-link" to='/' onClick={logout}>Logout</Link>
+                </li>
+              </>
+              :
+              <li className="nav-item">
+                <Link className="nav-link" to='/' onClick={login}>Login</Link>
+              </li>
+            }
+            {user ?
+              <img id="user-pic" src={user.photoURL} alt={user.displayName} />
+              :
+              null
+            }
+          </ul>
+        </div>
       </div>
     </nav>
   )
