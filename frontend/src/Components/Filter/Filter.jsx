16347295f0723dc79efc6370fb695b2176863144
Peter Lam <plam1216@gmail.com>
2022-11-17 00:07:59 -0500

    update filter styling

--- frontend/src/Components/Filter/Filter.jsx
+++ frontend/src/Components/Filter/Filter.jsx
@@ -3,23 +3,36 @@ import React from 'react'
 const Filter = ({ handleChange, handleSubmit }) => {
     return (
         <>
-            <h1>Filter</h1>
-            <div className="filter-form">
-                <form onSubmit={handleSubmit} className="col-3">
-                    <select onChange={handleChange} className="form-select form-select-sm" aria-label=".form-select-sm example">
-                        <option value="All">All</option>
-                        <option value="S">S</option>
-                        <option value="M">M</option>
-                        <option value="L">L</option>
-                        <option value="XL">XL</option>
-                    </select>
+            <h5 className="text-start fw-bold">Filter</h5>
 
-                    <button
-                        className="btn btn-secondary"
-                        type="submit"
-                    >
-                        Submit
-                    </button>
+            <div className="filter-form text-start">
+                <form
+                    onSubmit={handleSubmit}
+                    className="row g-2"
+                >
+                    <div className="col-3">
+                        <select
+                            onChange={handleChange}
+                            className="form-select form-select-sm"
+                            aria-label=".form-select-sm example"
+                        >
+                            <option value="All">All</option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                        </select>
+                    </div>
+
+                    <div className="col-2">
+
+                        <button
+                            className="btn btn-sm btn-secondary"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </form>
             </div>
         </>
